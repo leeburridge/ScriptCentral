01fe2abbd09b545b67313a9eb762227c9c79c33b
Lee Burridge <6240290+leeburridge@users.noreply.github.com>
2017-10-04 10:32:12 +0100

    Create ipscan.bat

---
+++ ipscan.bat
@@ -0,0 +1,43 @@
+@echo off
+
+echo ipScan (C)2017 Anthony Burridge
+if %1.==. (
+    echo USAGE : ipscan.bat ipsubnet filename.txt
+) else (
+    call:srange %1 %2
+)
+goto:eof
+
+:srange
+if %2.==. (
+    echo USAGE : ipscan.bat ipsubnet filename.txt
+    echo Please specify a filename
+    goto:eof
+) 
+
+set "ip=%1"
+SET "offsets=0.0.0.-24"
+
+for /f "tokens=1-4 delims=. " %%a in ("%ip%") do (
+set octetA=%%a
+set octetB=%%b
+set octetC=%%c
+set octetD=%%d
+)
+FOR /f "tokens=1-4 delims=." %%a in ("%offsets%") do (
+SET /a octetA+=%%a
+SET /a octetB+=%%b
+SET /a octetC+=%%c
+SET /a octetD+=%%d
+)
+
+set iprange=%octetA%.%octetB%.%octetC%
+set ipout=%iprange%.0/24
+echo Scanning %ipout%
+
+set iprange=%iprange%.%%i
+echo This will take some time...
+echo ipScan results - %ipout%>%2
+FOR /L %%i IN (1,1,254) DO ping -n 1 %iprange% | FIND /i "Reply">>%2
+echo Finished.
+goto:eof
