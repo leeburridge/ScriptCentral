8df5014838c5151d0993aa084e1e876c038364d2
Lee Burridge <6240290+leeburridge@users.noreply.github.com>
2017-11-08 11:27:49 +0000

    Update and rename del-mbox.ps1 to connect-365.ps1

--- del-mbox/connect-365.ps1
+++ del-mbox/connect-365.ps1
@@ -4,6 +4,3 @@ $UserCredential = Get-Credential
 $Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $UserCredential -Authentication Basic -AllowRedirection
 
 Import-PSSession $Session
-
-Remove-Mailbox -Identity "Walter Harp"
-Remove-MsolUser -UserPrincipalName <Walter Harp> -RemoveFromRecycleBin true
