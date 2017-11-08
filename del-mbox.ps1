d699b3bbd1b3d0a3b6c437d30d34cb7c94875fa3
Lee Burridge <6240290+leeburridge@users.noreply.github.com>
2017-11-08 11:21:09 +0000

    Delete del-mbox.ps1

--- del-mbox.ps1
+++
@@ -1,9 +0,0 @@
-Set-ExecutionPolicy RemoteSigned
-$UserCredential = Get-Credential
-
-$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $UserCredential -Authentication Basic -AllowRedirection
-
-Import-PSSession $Session
-
-Remove-Mailbox -Identity "Walter Harp"
-Remove-MsolUser -UserPrincipalName <Walter Harp> -RemoveFromRecycleBin true
