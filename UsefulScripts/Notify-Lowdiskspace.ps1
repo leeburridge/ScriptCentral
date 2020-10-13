fdd2497c5a52ed0aceed62a2bab8697ed8b79683
Lee Burridge <6240290+leeburridge@users.noreply.github.com>
2020-10-13 13:59:02 +0100

    Create Notify-Lowdiskspace.ps1

---
+++ UsefulScripts/Notify-Lowdiskspace.ps1
@@ -0,0 +1,22 @@
+' Change variables to be working SMTP creds and the $cname to the PCs name. Much more to be configured in here and needs putting into arguments.
+
+$cname="Mycomputer"
+ForEach ($c in $cname)
+{
+$disk=Get-WmiObject win32_logicaldisk -ComputerName $c -Filter "Drivetype=3" -ErrorAction SilentlyContinue | Where-Object {($_.freespace/$_.size) -le '0.05'}
+If ($disk)
+{
+$EmailToAdd = "test@test.com"
+$EmailFromAdd = "test@test.com"
+$userdet = 'testuser'
+$passworddet = "testpwd"
+$Subjectdet = "Disk space alert"
+$Bodydet = "low space in the system"
+$SMTPServerdet = "testswer"
+$SMTPMessagedet = New-Object System.Net.Mail.MailMessage($EmailFromAdd,$EmailToAdd,$Subjectdet,$Bodydet)
+$SMTPClientdet = New-Object Net.Mail.SmtpClient($SMTPServerdet, 587)
+$ SMTPClientdet.EnableSsl = $true
+$ SMTPClientdet.Credentials = New-Object System.Net.NetworkCredential($userdet, $passworddet)
+$ SMTPClientdet.Send($SMTPMessagedet)
+}
+}
