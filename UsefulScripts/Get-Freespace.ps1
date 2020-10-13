1ff494df5a5290a18c854059ea29412a2d1d5501
Lee Burridge <6240290+leeburridge@users.noreply.github.com>
2020-10-13 13:54:02 +0100

    Create Get-Freespace.ps1

---
+++ UsefulScripts/Get-Freespace.ps1
@@ -0,0 +1,8 @@
+' Simple script to get free drive space on ALL drives
+
+$Drive=Get-WmiObject Win32_LogicalDisk -Filter "DriveType = 3"
+$DriveSize=$Drive.Size;$DriveSize=[math]::Round($DriveSize/1GB)
+$FreeSpace=$Drive.FreeSpace;$FreeSpace=[math]::Round($FreeSpace/1GB)
+$DriveName=$Drive.Name
+$ComputerName=Get-WmiObject Win32_ComputerSystem;$ComputerName=$ComputerName.Name
+$UsedSpace=$DriveSize - $FreeSpace;$UsedSpace=[string]$UsedSpace+" GB free on drive $DriveName on computer $ComputerName"
