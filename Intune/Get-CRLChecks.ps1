a33d89386d0ac7ad2e64c1ac34635858349fa32a
Lee Burridge <6240290+leeburridge@users.noreply.github.com>
2021-04-29 10:28:01 +0000

    Modified
    Fixed so that email sends better data and does more testing.

--- Intune/Get-CRLChecks.ps1
+++ Intune/Get-CRLChecks.ps1
@@ -1,8 +1,12 @@
-# Very messy bit of quick code that reports on CRL filest that have
-# an expiry happening in the next 2 days
+﻿# Get-CRLChecks.ps1
+# Script reads certs in an Azure blob and checks for validity. Not the most eloquant solutions but it works.
 
-# (C) 2021 Lee Burridge
+# If a cert is going to expire within the next 2 days it will email
 
+write-host "Get-CRLChecks (C)2021 Lee Burridge"
+
+$debugswitch = 1
+$EmailTo = "lburridge@centrality.com"
 
 Function Get-CRLTimeValidity {     
                                                                                                                                   
@@ -19,7 +23,9 @@ Function Get-CRLTimeValidity {
                                                                                                                                                                 
     )                                                                                                                                                           
     process{                 
-                                                                                                                               
+		if ($debugswitch -eq 2 ) {
+			write-host "Checking $CRL"								
+		}			
         #Import the CRL file or byte array                                                                                                                      
         if (-not ($CRLBytes = Get-content $CRL -Encoding byte -ErrorAction SilentlyContinue)){                                                                  
             write-verbose "CRL file not found...assuming CRL raw bytes."                                                                                        
@@ -61,23 +67,27 @@ Function Get-CRLTimeValidity {
 
         $todaydate = get-date -format "dd/MM/yyyy"
 
+       # $todaydate = $todaydate -format "dd/MM/yyyy"
+
         $diff = new-timespan -start $todaydate -end $dedate
 
         $dfdays = $diff.Days
-                                                                                                                                                                     
-        $isvalid = ($nextUpdate -gt (get-date))                                                                                                                 
-        if ($dfdays[0] -lt 3) {
-	$global:CAN = $CAName + "`r`n"
-	$global:CAN2 = "Expires in (days) : " + $dfdays	+ "`r`n"
-	$global:CAN3 = "Next Update : " + $NextUpdate + "`r`n"
-                                                                                                                                               
-        [pscustomobject]@{                                                                                                                                      
-	    CAName = $CAName                                                                                                                                   
-            ThisUpdate = $ThisUpdate                                                                                                                            
-            NextUpdate = $NextUpdate                                                                                                                            
-            isValid = $isvalid    
-            DaysUntilExpiry = $dfdays                                                                                                                              
-        }    
+		#write-host $dfdays
+                	                                                                                                                                                     
+        $isvalid = ($nextUpdate -gt (get-date)) 
+		
+        if ($dfdays -lt 2) {
+			$global:CAN = $CAName + "`r`n"
+			$global:CAN2 = "Expires in (days) : " + $dfdays	+ "`r`n"
+			$global:CAN3 = "Next Update : " + $NextUpdate + "`r`n"
+	                                                                                                                                               
+			[pscustomobject]@{                                                                                                                                      
+				CAName = $CAName                                                                                                                                   
+				ThisUpdate = $ThisUpdate                                                                                                                            
+				NextUpdate = $NextUpdate                                                                                                                            
+				isValid = $isvalid    
+				DaysUntilExpiry = $dfdays                                                                                                                              
+			}    
         }                                                                                                                                                   
     }                                                                                                                                                           
 }                                  
@@ -98,53 +108,223 @@ Function Get-CRLFile {
     
 }
 
-Get-CRLFile 'link to crl in azure blob' file1.crl
+Get-CRLFile 'https://cpdpublicresources.blob.core.windows.net/121f63f0-df30-4117-b5c0-e5bdec17da2a/pki/COKETHORPE-ROOT+.crl' COKETHORPE+.crl
+Get-CRLFile 'https://cpdpublicresources.blob.core.windows.net/121f63f0-df30-4117-b5c0-e5bdec17da2a/pki/COKETHORPE-ROOT.crl' COKETHORPE.crl
+Get-CRLFile 'https://cpdpublicresources.blob.core.windows.net/165c1e90-512a-4196-bbd1-94cc6c6a4f52/pki/WSH Certificate Authority+.crl' WSH+.crl
+Get-CRLFile 'https://cpdpublicresources.blob.core.windows.net/165c1e90-512a-4196-bbd1-94cc6c6a4f52/pki/WSH Certificate Authority.crl' WSH.crl
+Get-CRLFile 'https://cpdpublicresources.blob.core.windows.net/3bb43502-95da-4a42-81e2-e6d902013624/pki/Welcome Break Certificate Authority+.crl' WB+.crl
+Get-CRLFile 'https://cpdpublicresources.blob.core.windows.net/3bb43502-95da-4a42-81e2-e6d902013624/pki/Welcome Break Certificate Authority.crl' WB.crl
+Get-CRLFile 'https://cpdpublicresources.blob.core.windows.net/af2909d1-51ab-425d-ac7f-bf6da47b3a59/pki/Marston CA+.crl' Marston+.crl
+Get-CRLFile 'https://cpdpublicresources.blob.core.windows.net/af2909d1-51ab-425d-ac7f-bf6da47b3a59/pki/Marston CA.crl' Marston.crl
+Get-CRLFile 'https://cpdpublicresources.blob.core.windows.net/e3689cab-dae4-4651-979f-48fee919607a/pki/Bordeaux Index Certificate Authority+.crl' bi+.crl
+Get-CRLFile 'https://cpdpublicresources.blob.core.windows.net/e3689cab-dae4-4651-979f-48fee919607a/pki/Bordeaux Index Certificate Authority.crl' bi.crl
 
 function get-allcrls {
 
 Write-output $spacer | Out-File c:\cert\out2.txt
 
-Get-CRLTimeValidity file1.crl
-Write-output $CAN | Out-File c:\cert\out2.txt -append
-Write-output "<br />" | Out-File c:\cert\out2.txt -append
-write-output $CAN2 | Out-File c:\cert\out2.txt -append 
-Write-output "<br />" | Out-File c:\cert\out2.txt -append
-write-output $CAN3 | Out-File c:\cert\out2.txt -append 
-Write-output "<br />" | Out-File c:\cert\out2.txt -append
-write-output $spacer | Out-File c:\cert\out2.txt -append 
-Write-output "<br />" | Out-File c:\cert\out2.txt -append
-$CAN=""
-$CAN2=""
-$CAN3=""
+Get-CRLTimeValidity COKETHORPE+.crl
+if ($global:CAN.length -ne 0) {
+	write-host "Cokethorpe+..expiry detected"
+	Write-output $global:CAN | Out-File c:\cert\out2.txt -append
+	Write-output "<br />" | Out-File c:\cert\out2.txt -append
+	write-output $global:CAN2 | Out-File c:\cert\out2.txt -append 
+	Write-output "<br />" | Out-File c:\cert\out2.txt -append
+	write-output $global:CAN3 | Out-File c:\cert\out2.txt -append 
+	Write-output "<br />" | Out-File c:\cert\out2.txt -append
+	write-output $spacer | Out-File c:\cert\out2.txt -append 
+	Write-output "<br />" | Out-File c:\cert\out2.txt -append
+} else {
+	write-host "COKETHORPE+..OK"
+}
+$global:CAN=""
+$global:CAN2=""
+$global:CAN3=""
 
+Get-CRLTimeValidity COKETHORPE.crl
+if ($global:CAN.length -ne 0) {
+	write-host "Cokethorpe..expiry detected"
+	Write-output $global:CAN | Out-File c:\cert\out2.txt -append
+	Write-output "<br />" | Out-File c:\cert\out2.txt -append
+	write-output $global:CAN2 | Out-File c:\cert\out2.txt -append 
+	Write-output "<br />" | Out-File c:\cert\out2.txt -append
+	write-output $global:CAN3 | Out-File c:\cert\out2.txt -append 
+	Write-output "<br />" | Out-File c:\cert\out2.txt -append
+	write-output $spacer | Out-File c:\cert\out2.txt -append 
+	Write-output "<br />" | Out-File c:\cert\out2.txt -append
+} else {
+	write-host "COKETHORPE..OK"
+}
+$global:CAN=""
+$global:CAN2=""
+$global:CAN3=""
+
+Get-CRLTimeValidity WSH+.crl 
+if ($global:CAN.length -ne 0) {
+	write-host "WSH+..expiry detected"
+	Write-output $global:CAN | Out-File c:\cert\out2.txt -append
+	Write-output "<br />" | Out-File c:\cert\out2.txt -append
+	write-output $global:CAN2 | Out-File c:\cert\out2.txt -append 
+	Write-output "<br />" | Out-File c:\cert\out2.txt -append
+	write-output $global:CAN3 | Out-File c:\cert\out2.txt -append 
+	Write-output "<br />" | Out-File c:\cert\out2.txt -append
+	write-output $spacer | Out-File c:\cert\out2.txt -append 
+	Write-output "<br />" | Out-File c:\cert\out2.txt -append
+} else {
+	write-host "WSH+..OK"
+}
+$global:CAN=""
+$global:CAN2=""
+$global:CAN3=""
+
+Get-CRLTimeValidity WSH.crl 
+if ($global:CAN.length -ne 0) {
+	Write-host "WSH..expiry detected"
+	Write-output $global:CAN | Out-File c:\cert\out2.txt -append
+	Write-output "<br />" | Out-File c:\cert\out2.txt -append
+	write-output $global:CAN2 | Out-File c:\cert\out2.txt -append 
+	Write-output "<br />" | Out-File c:\cert\out2.txt -append
+	write-output $global:CAN3 | Out-File c:\cert\out2.txt -append 
+	Write-output "<br />" | Out-File c:\cert\out2.txt -append
+	write-output $spacer | Out-File c:\cert\out2.txt -append 
+	Write-output "<br />" | Out-File c:\cert\out2.txt -append
+} else {
+	write-host "WSH..OK"
+}
+$global:CAN=""
+$global:CAN2=""
+$global:CAN3=""
+
+Get-CRLTimeValidity WB+.crl 
+if ($global:CAN.length -ne 0) {
+	write-host "WB+..expiry detected"
+	Write-output $global:CAN | Out-File c:\cert\out2.txt -append
+	Write-output "<br />" | Out-File c:\cert\out2.txt -append
+	write-output $global:CAN2 | Out-File c:\cert\out2.txt -append 
+	Write-output "<br />" | Out-File c:\cert\out2.txt -append
+	write-output $global:CAN3 | Out-File c:\cert\out2.txt -append 
+	Write-output "<br />" | Out-File c:\cert\out2.txt -append
+	write-output $spacer | Out-File c:\cert\out2.txt -append 
+	Write-output "<br />" | Out-File c:\cert\out2.txt -append
+} else {
+	write-host "WB+..OK"
+}
+$global:CAN=""
+$global:CAN2=""
+$global:CAN3=""
+
+Get-CRLTimeValidity WB.crl 
+if ($global:CAN.length -ne 0) {
+	write-host "WB..expiry detected"
+	Write-output $global:CAN | Out-File c:\cert\out2.txt -append
+	Write-output "<br />" | Out-File c:\cert\out2.txt -append
+	write-output $global:CAN2 | Out-File c:\cert\out2.txt -append 
+	Write-output "<br />" | Out-File c:\cert\out2.txt -append
+	write-output $global:CAN3 | Out-File c:\cert\out2.txt -append 
+	Write-output "<br />" | Out-File c:\cert\out2.txt -append
+	write-output $spacer | Out-File c:\cert\out2.txt -append 
+	Write-output "<br />" | Out-File c:\cert\out2.txt -append
+} else {
+	write-host "WB..OK"
+}
+$global:CAN=""
+$global:CAN2=""
+$global:CAN3=""
+
+Get-CRLTimeValidity Marston+.crl 
+if ($global:CAN.length -ne 0) {
+	write-host "MARSTON+..expiry detected"
+	Write-output $global:CAN | Out-File c:\cert\out2.txt -append
+	Write-output "<br />" | Out-File c:\cert\out2.txt -append
+	write-output $global:CAN2 | Out-File c:\cert\out2.txt -append 
+	Write-output "<br />" | Out-File c:\cert\out2.txt -append
+	write-output $global:CAN3 | Out-File c:\cert\out2.txt -append 
+	Write-output "<br />" | Out-File c:\cert\out2.txt -append
+	write-output $spacer | Out-File c:\cert\out2.txt -append 
+	Write-output "<br />" | Out-File c:\cert\out2.txt -append
+} else {
+	write-host "MARSTON+..OK"
+}
+$global:CAN=""
+$global:CAN2=""
+$global:CAN3=""
+
+Get-CRLTimeValidity Marston.crl 
+if ($global:CAN.length -ne 0) {
+	write-host "MARSTON..expiry detected"
+	Write-output $global:CAN | Out-File c:\cert\out2.txt -append
+	Write-output "<br />" | Out-File c:\cert\out2.txt -append
+	write-output $global:CAN2 | Out-File c:\cert\out2.txt -append 
+	Write-output "<br />" | Out-File c:\cert\out2.txt -append
+	write-output $global:CAN3 | Out-File c:\cert\out2.txt -append 
+	Write-output "<br />" | Out-File c:\cert\out2.txt -append
+	write-output $spacer | Out-File c:\cert\out2.txt -append 
+	Write-output "<br />" | Out-File c:\cert\out2.txt -append
+} else {
+	write-host "MARSTON..OK"
+}
+$global:CAN=""
+$global:CAN2=""
+$global:CAN3=""
+
+Get-CRLTimeValidity BI+.crl 
+if ($global:CAN.length -ne 0) {
+	write-Host "BI+..expiry detected"
+	Write-output $global:CAN | Out-File c:\cert\out2.txt -append
+	Write-output "<br />" | Out-File c:\cert\out2.txt -append
+	write-output $global:CAN2 | Out-File c:\cert\out2.txt -append 
+	Write-output "<br />" | Out-File c:\cert\out2.txt -append
+	write-output $global:CAN3 | Out-File c:\cert\out2.txt -append 
+	Write-output "<br />" | Out-File c:\cert\out2.txt -append
+	write-output $spacer | Out-File c:\cert\out2.txt -append 
+	Write-output "<br />" | Out-File c:\cert\out2.txt -append
+} else {
+	write-host "BI+..OK"
+}
+$global:CAN=""
+$global:CAN2=""
+$global:CAN3=""
+
+Get-CRLTimeValidity BI.crl 
+if ($global:CAN.length -ne 0) {
+	write-host "BI..expiry detected"
+	Write-output $global:CAN | Out-File c:\cert\out2.txt -append
+	Write-output "<br />" | Out-File c:\cert\out2.txt -append
+	write-output $global:CAN2 | Out-File c:\cert\out2.txt -append 
+	Write-output "<br />" | Out-File c:\cert\out2.txt -append
+	write-output $global:CAN3 | Out-File c:\cert\out2.txt -append 
+	Write-output "<br />" | Out-File c:\cert\out2.txt -append
+	write-output $spacer | Out-File c:\cert\out2.txt -append 
+	Write-output "<br />" | Out-File c:\cert\out2.txt -append
+} else {
+	write-host "BI..OK"
+}
+$global:CAN=""
+$global:CAN2=""
+$global:CAN3=""
 }
 
 get-allcrls > out.txt
-# get-date >> out.txt
-# $out > out.txt
-$output = get-allcrls
+
+#$output = get-allcrls
 
 $currentTime = Get-Date -format "dd MMM yyyy"
 
 $crls = get-content out2.txt
 
-# SMTP Server login details
+
 $User = "monitor@digitalsaviour.co.uk"
 $File = "C:\cert\emailpass.txt"
-
 $cred=New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $User, (Get-Content $File | ConvertTo-SecureString)
 
-# populate with email address that will receive the report 
-$EmailTo = ""
-
-# email address of sender of report
-$EmailFrom = ""
+$EmailFrom = "monitor@digitalsaviour.co.uk"
 $Subject = "CRL Check " + $currentTime 
+if ($crls.length -ne 0) {
 $Body = $crls
-
-# SMTP server details
+} else {
+$Body = "No certificates due to expire"
+}
 $SMTPServer = "mail.digitalsaviour.co.uk" 
-
 #$filenameAndPath = "C:\cert\out.txt"
 $SMTPMessage = New-Object System.Net.Mail.MailMessage($EmailFrom,$EmailTo,$Subject,$Body)
 $SMTPMessage.IsBodyHtml = $true
@@ -155,4 +335,8 @@ $SMTPClient.EnableSsl = $true
 $SMTPClient.Credentials = New-Object System.Net.NetworkCredential($cred.UserName, $cred.Password); 
 $SMTPClient.Send($SMTPMessage)
 
+if ($debugswitch = 2) {
+	Write-Host "Completed and sent email to $EmailTo"
+	$SMTPMessage > email.txt
+}
 
