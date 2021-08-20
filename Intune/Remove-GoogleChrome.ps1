ad3f3ca4072bb84c58dd60f67a20df2f6c3e1abb
Lee Burridge <6240290+leeburridge@users.noreply.github.com>
2021-08-20 12:01:36 +0100

    Update Remove-GoogleChrome.ps1

--- Intune/Remove-GoogleChrome.ps1
+++ Intune/Remove-GoogleChrome.ps1
@@ -1,3 +1,5 @@
+# Script to uninstall Google Chrome. Can be used in Intune to remove if necessary.
+
 $GoogleChrome = [CimInstance](Get-CimInstance -ClassName Win32_Product | Where-Object {$_.Name -eq 'Google Chrome'})
 If ($GoogleChrome -ne $null) {
     Write-Host 'Uninstalling Google Chrome'$GoogleChrome.Version
