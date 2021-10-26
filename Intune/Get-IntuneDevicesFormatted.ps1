ece5f74d4022b2b51605b5dce404f318a4719f7b
Lee Burridge <6240290+leeburridge@users.noreply.github.com>
2021-10-26 15:33:38 +0100

    Update Get-IntuneDevicesFormatted.ps1

--- Intune/Get-IntuneDevicesFormatted.ps1
+++ Intune/Get-IntuneDevicesFormatted.ps1
@@ -1,5 +1,20 @@
+param ($company)
+
+write-host -foregroundcolor darkgreen "Get-IntuneDevicesFormatted"
+
+if ($company -eq $null){
+  echo "Usage : Get-IntuneDevicesFormatter.ps1 -company <name of company>"
+  exit 1
+}
+
+$outfile = $company + "-" + (get-date).ToString("M") + ".csv"
+
+
 set-executionpolicy -executionpolicy bypass
 import-module microsoft.graph.intune
 connect-msgraph -adminconsent
 
-get-IntuneManagedDevice | select deviceName, lastSyncDateTime, enrolledDateTime, complianceState, operatingSystem, osVersion, serialNumber, manufacturer, model, userDisplayName | export-csv devices.csv
+get-IntuneManagedDevice | select deviceName, lastSyncDateTime, enrolledDateTime, complianceState, operatingSystem, osVersion, serialNumber, manufacturer, model, userDisplayName | export-csv $outfile
+
+echo "Output to $outfile"
+exit 0
