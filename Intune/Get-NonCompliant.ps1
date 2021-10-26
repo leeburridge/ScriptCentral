52b05ff4f7c36c45ef50bda6e232ba0d3aca182a
Lee Burridge <6240290+leeburridge@users.noreply.github.com>
2021-10-26 17:02:41 +0100

    Update Get-NonCompliant.ps1

--- Intune/Get-NonCompliant.ps1
+++ Intune/Get-NonCompliant.ps1
@@ -6,6 +6,19 @@ Connect-MSGraph -AdminConsent
 #Show how many enrolled devices there are
 Get-IntuneManagedDeviceOverview | select-object -property enrolledDeviceCount
 
+# Display active devices (Since a certain date)
+active = Get-IntuneManagedDevice | Where-Object { $_.lastSyncDateTime -gt "10/1/2021 0:00:01 AM" }
+$active.count
+
+# Noncompliant count
+$noncompliant = Get-IntuneManagedDevice -Filter "complianceState eq 'noncompliant'" | select-object -property managedDeviceOwnerType,complianceState,deviceName,serialNumber,lastSyncDateTime,osVersion,manufacturer,model,userPrincipalName,userDisplayName
+$noncompliant.count
+
+# Compliant count
+$compliant = Get-IntuneManagedDevice -Filter "complianceState eq 'compliant'" | select-object -property managedDeviceOwnerType,complianceState,deviceName,serialNumber,lastSyncDateTime,osVersion,manufacturer,model,userPrincipalName,userDisplayName
+$compliant.count
+
+############################################## Junk notes
 # List all non compliant devices registered into Intune
 Get-IntuneManagedDevice -Filter "complianceState eq 'noncompliant'" | select-object -property managedDeviceOwnerType,complianceState,deviceName,serialNumber,lastSyncDateTime,osVersion,manufacturer,model,userPrincipalName,userDisplayName
 
@@ -24,7 +37,3 @@ Get-IntuneDeviceConfigurationPolicy
 # 
 $polID = Get-IntuneDeviceCompliancePolicy -filter "displayname eq '##GROUP##'" | select-object -ExpandProperty id
 Get-IntuneDeviceCompliancePolicyDeviceStatus -deviceCompliancePolicyId "$polID" -Filter {status -ne "compliant"}
-
-# Display active devices (Since a certain date)
-active = Get-IntuneManagedDevice | Where-Object { $_.lastSyncDateTime -gt "10/1/2021 0:00:01 AM" }
-$active.count
