6a46f99841c0f9d15ddcaafce2f32ce14fd235cd
Lee Burridge <6240290+leeburridge@users.noreply.github.com>
2021-10-26 17:13:24 +0100

    Update Get-NonCompliant.ps1

--- Intune/Get-NonCompliant.ps1
+++ Intune/Get-NonCompliant.ps1
@@ -5,19 +5,22 @@ Connect-MSGraph -AdminConsent
 
 #Show how many enrolled devices there are
 $enroled = Get-IntuneManagedDeviceOverview | select-object -property enrolledDeviceCount
-echo "Enroled : $enroled"
+write-host $Enroled
 
 # Display active devices (Since a certain date)
-active = Get-IntuneManagedDevice | Where-Object { $_.lastSyncDateTime -gt "10/1/2021 0:00:01 AM" }
-echo "Active devices : $active.count"
+$active = Get-IntuneManagedDevice | Where-Object { $_.lastSyncDateTime -gt "10/1/2021 0:00:01 AM" }
+echo "Active :"
+$active.count
 
 # Noncompliant count
 $noncompliant = Get-IntuneManagedDevice -Filter "complianceState eq 'noncompliant'" | select-object -property managedDeviceOwnerType,complianceState,deviceName,serialNumber,lastSyncDateTime,osVersion,manufacturer,model,userPrincipalName,userDisplayName
-echo "Non-Compliant : $noncompliant.count"
+echo "Non compliant :"
+$noncompliant.count
 
 # Compliant count
 $compliant = Get-IntuneManagedDevice -Filter "complianceState eq 'compliant'" | select-object -property managedDeviceOwnerType,complianceState,deviceName,serialNumber,lastSyncDateTime,osVersion,manufacturer,model,userPrincipalName,userDisplayName
-echo "Compliant : $compliant.count"
+echo "Compliant :"
+$compliant.count
 
 ############################################## Junk notes
 # List all non compliant devices registered into Intune
