1e7ba595fbe0429a1fe9d33715ba76a95efcfc7f
Lee Burridge <6240290+leeburridge@users.noreply.github.com>
2021-10-26 15:16:52 +0100

    Update Get-IntuneDevicesFormatted.ps1
    Adapted

--- Intune/Get-IntuneDevicesFormatted.ps1
+++ Intune/Get-IntuneDevicesFormatted.ps1
@@ -1,10 +1,5 @@
-$tenant = "a31f2452-5c8f-426a-8bb1-432d7ddc65a7"
-$authority = "https://login.windows.net/$tenant"
-$clientId = "b8ac7218-3f2f-4bdf-b738-226a8cd8ad47"
-$Thumbprint = "4f8d9229b16c265b7924652056c2bdfdfc842ffe"
+set-executionpolicy -executionpolicy bypass
+import-module microsoft.graph.intune
+connect-msgraph -adminconsent
 
-Update-MSGraphEnvironment -AppId $clientId -Quiet
-Update-MSGraphEnvironment -AuthUrl $authority -Quiet
-Connect-MSGraph -CertificateThumbprint $Thumbprint
-
-get-IntuneManagedDevice | select deviceName, lastSyncDateTime, enrolledDateTime, complianceState, operatingSystem, osVersion, serialNumber, userDisplayName, manufacturer, model | export-csv devices.csv
+get-IntuneManagedDevice | select deviceName, lastSyncDateTime, enrolledDateTime, complianceState, operatingSystem, osVersion, serialNumber, manufacturer, model, userDisplayName | export-csv devices.csv
