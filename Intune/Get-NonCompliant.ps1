3257203dba29458648d94bebc5ac2461561ab231
Lee Burridge <6240290+leeburridge@users.noreply.github.com>
2021-10-27 09:52:11 +0100

    Update Get-NonCompliant.ps1

--- Intune/Get-NonCompliant.ps1
+++ Intune/Get-NonCompliant.ps1
@@ -1,24 +1,24 @@
 Install-Module -Name Microsoft.Graph.Intune
 Connect-MSGraph -AdminConsent
 
-# Run this to get non compliant devices and reasons
+# Run this to get non compliant devices and reasons (Excludes VMs)
 
 # Show how many enrolled devices there are
 $enroled = Get-IntuneManagedDeviceOverview | select-object -property enrolledDeviceCount
 write-host "- $Enroled -"
 
 # Display active devices (Since a certain date)
-$active = Get-IntuneManagedDevice | Where-Object { $_.lastSyncDateTime -gt "10/1/2021 0:00:01 AM" }
+$active = Get-IntuneManagedDevice | Where-Object { $_.lastSyncDateTime -gt "10/1/2021 0:00:01 AM" } | Where-Object { $_.model -ne "Virtual Machine" }
 echo "Active :"
 $active.count
 
 # Noncompliant count
-$noncompliant = Get-IntuneManagedDevice -Filter "complianceState eq 'noncompliant'" | select-object -property managedDeviceOwnerType,complianceState,deviceName,serialNumber,lastSyncDateTime,osVersion,manufacturer,model,userPrincipalName,userDisplayName
+$noncompliant = Get-IntuneManagedDevice -Filter "complianceState eq 'noncompliant'" | select-object -property managedDeviceOwnerType,complianceState,deviceName,serialNumber,lastSyncDateTime,osVersion,manufacturer,model,userPrincipalName,userDisplayName | Where-Object { $_.model -ne "Virtual Machine" }
 echo "Non compliant :"
 $noncompliant.count
 
 # Compliant count
-$compliant = Get-IntuneManagedDevice -Filter "complianceState eq 'compliant'" | select-object -property managedDeviceOwnerType,complianceState,deviceName,serialNumber,lastSyncDateTime,osVersion,manufacturer,model,userPrincipalName,userDisplayName
+$compliant = Get-IntuneManagedDevice -Filter "complianceState eq 'compliant'" | select-object -property managedDeviceOwnerType,complianceState,deviceName,serialNumber,lastSyncDateTime,osVersion,manufacturer,model,userPrincipalName,userDisplayName | Where-Object { $_.model -ne "Virtual Machine" }
 echo "Compliant :"
 $compliant.count
 
