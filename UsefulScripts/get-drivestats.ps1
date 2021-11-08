0b9b9907b03b5cada18f5880ce09ba9d2ed1f8df
Lee Burridge <6240290+leeburridge@users.noreply.github.com>
2021-11-08 19:14:41 +0000

    Create get-drivestats.ps1

---
+++ UsefulScripts/get-drivestats.ps1
@@ -0,0 +1,37 @@
+# gets all drives from the below listed servers and outputs to CSVs
+#
+
+$servers = 'server1', 'server2'
+
+
+for ($servercount=1; $servercount -le $servers.count; servercount++){
+    $s = New-PSSession -ComputerName $servers[$servercount]
+    Enter-PSSession -Session $s
+
+    $Drive = get-psdrive -psprovider FileSystem | select root
+
+    $drivecount=$drive.count-1
+
+    for ($i=0; $i -le $drivecount; $i++){
+        $drive[$i]
+        $targetfolder=$drive[$i].root
+        $dataColl = @()
+        $dataColl = @()
+        gci -force $targetfolder -ErrorAction SilentlyContinue | ? { $_ -is [io.directoryinfo] } | % {
+        $len = 0
+        gci -recurse -force $_.fullname -ErrorAction SilentlyContinue | % { $len += $_.length }
+        $foldername = $_.fullname
+        $foldersize= '{0:N2}' -f ($len / 1Gb)
+        $dataObject = New-Object PSObject
+        Add-Member -inputObject $dataObject -memberType NoteProperty -name “foldername” -value $foldername
+        Add-Member -inputObject $dataObject -memberType NoteProperty -name “foldersizeGb” -value $foldersize
+        $dataColl += $dataObject
+        }
+        $dataColl | Out-GridView -Title “Size of subdirectories”
+
+        $outfile = $s+"-Drive"+$i"+".csv"
+        $datacoll | export-csv -path $outfile
+  
+    }
+    Exit-PSSession $s
+}
