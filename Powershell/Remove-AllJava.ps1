655d520a8243d9cf8535c5851d0e45e23c1eea7d
Lee Burridge <6240290+leeburridge@users.noreply.github.com>
2022-10-28 10:49:29 +0100

    Add files via upload

---
+++ Powershell/Remove-AllJava.ps1
@@ -0,0 +1,4 @@
+$Source = Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*
+$Uninstall = $Source | Where-Object {($_.Publisher -like '*Oracle*')-and ($_.DisplayName -like 'Java*')} | select UninstallString
+$UninstallStrings = $Uninstall.UninstallString -replace "MsiExec.exe ","MsiExec.exe /qn " -replace "/I","/X"
+ForEach($UninstallString in $UninstallStrings){& cmd /c "$UninstallString"}
