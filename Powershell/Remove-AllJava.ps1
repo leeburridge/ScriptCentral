a50455dc6546231499349bc17e1d44122e704c79
Lee Burridge <6240290+leeburridge@users.noreply.github.com>
2022-10-28 10:49:58 +0100

    Update Remove-AllJava.ps1

--- Powershell/Remove-AllJava.ps1
+++ Powershell/Remove-AllJava.ps1
@@ -1,3 +1,5 @@
+# This little script will remove Java from a system
+
 $Source = Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*
 $Uninstall = $Source | Where-Object {($_.Publisher -like '*Oracle*')-and ($_.DisplayName -like 'Java*')} | select UninstallString
 $UninstallStrings = $Uninstall.UninstallString -replace "MsiExec.exe ","MsiExec.exe /qn " -replace "/I","/X"
